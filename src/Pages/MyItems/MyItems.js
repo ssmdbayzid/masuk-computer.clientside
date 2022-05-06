710e043afd7408b39cbf1054d52378453fcb9b9e
ssmdbayzid <ssmd.bayzid@gmail.com>
2022-05-06 15:01:21 +0600

    added json file

--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+
 
 const MyItems = () => {
+    const [products, setProducts] = useState();
+
+    useEffect(()=>{
+        fetch('product.json')
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+    },[])
     return (
         <div>
-            <h2>This is my ite,ms</h2>
+            <h2>This is my Total </h2>
+            <h2>This is my Total</h2>
+            {
+                products.map(product=><li>{product.name}</li>)
+            }
+            
+            
         </div>
     );
 };
